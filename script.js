3d33f91fec389da901e6cafd35a18809b438dc5f
muno17 <munozd17@gmail.com>
2023-03-23 17:58:55 -0400

    add four basic operator functions to js

---
+++ script.js
@@ -0,0 +1,15 @@
+function add(x, y) {
+    return x + y;
+}
+
+function subtract(x, y) {
+    return x - y;
+}
+
+function multiply(x, y) {
+    return x * y;
+}
+
+function divide(x, y) {
+    return x /y;
+}
