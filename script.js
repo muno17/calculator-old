1dc9f0ac942eae22529cbf2c85e983b497a6669f
muno17 <munozd17@gmail.com>
2023-03-24 16:38:27 -0400

    add initial functionality for dual display

--- script.js
+++ script.js
@@ -8,18 +8,6 @@ let signY = '+';
 let displayValue = document.getElementById('main');
 let displaySolution = document.getElementById('solution');
 
-function numberDisplay(num) {
-    if (displayValue.innerHTML.length > 50) {
-        return;
-    } else {
-        if (displayValue.innerHTML.length === 1 && displayValue.innerHTML === '0') {
-        displayValue.innerHTML = num;
-        } else {
-            displayValue.innerHTML += num;
-        }
-    }
-};
-
 // listen for a button to be pressed
 // add buttons to displayValue until operator is pressed
 let calcNumber= document.querySelectorAll('.number');
@@ -27,6 +15,20 @@ let calcNumber= document.querySelectorAll('.number');
 calcNumber.forEach(num => {
     num.addEventListener('click', () => numberDisplay(num.innerHTML, signX, signY))
 });
+
+function numberDisplay(num) {
+    if (displayValue.innerHTML.length > 25) {
+        return;
+    } else {
+        if (displayValue.innerHTML.length === 1 && displayValue.innerHTML === '0') {
+        displayValue.innerHTML = num;
+        } else {
+            
+            displayValue.innerHTML += num;
+            console.log('x value in num display = ' + x)
+        }
+    }
+};
 
 // listen for next buttons to be pressed
 // add buttons to displayValue until = is pressed
@@ -51,29 +53,35 @@ equals.addEventListener('click', () => {
 let operator = document.querySelectorAll('.operator');
 operator.forEach(oper => {
     oper.addEventListener('click', () => {
-
+        console.log('x going into operator = ' + x)
         // if op already has a value, run operation first and assign value to x
         if (op !== 0) {
             y = displayValue.innerHTML;
             // run operate() and assign to x
-            displayValue.innerHTML = operate(op, x, y);
+            displayValue.innerHTML = operate(op, x, y) + ' = ';
             x = displayValue.innerHTML;
-            
+            console.log('y val is = ' + y)
             displaySolution.innerHTML = x;
         } else {
             x = displayValue.innerHTML;
             displayValue.innerHTML = 0;
         }
-
+        console.log('x value in middle of operator = ' + x)
         // check which operator to assign to op
         if (oper.innerHTML === '+') {
+            
+            console.log('x value going out of operator = ' + x);
             op = 'add';
+            displayValue.innerHTML = displayValue.innerHTML + ' + '
         } else if (oper.innerHTML === '-') {
             op = 'subtract';
+            displayValue.innerHTML = displayValue.innerHTML + ' - '
         } else if (oper.innerHTML === '*') {
             op = 'multiply';
+            displayValue.innerHTML = displayValue.innerHTML + ' * '
         } else if (oper.innerHTML === '/') {
             op = 'divide';
+            displayValue.innerHTML = displayValue.innerHTML + ' / '
         };
     });
 });
@@ -126,11 +134,17 @@ deleteButton.addEventListener('click', () => {
 
 
 function operate(op, x, y) {
+    console.log('x value going into operate = ' + x)
+    console.log('y value going into operate = ' + y)
     x = parseFloat(x);
-    y = parseFloat(y);
     z = 0;
 
+
     if (op === 'add') {
+        let addArr = displayValue.innerHTML.split('+')
+        let y = parseFloat(addArr[1]); 
+        console.log('x value going out of operate() = ' + x);
+        console.log('y value going out of operate() = ' + y);
         z = add(x, y);
     } else if (op === 'subtract') {
         z = subtract(x, y);
