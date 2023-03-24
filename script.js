2ea3e9ba86cf1c275065d898373ddf2585735a7f
muno17 <munozd17@gmail.com>
2023-03-23 20:32:34 -0400

    add js for displayValue

--- script.js
+++ script.js
@@ -2,7 +2,13 @@ let x = 0;
 let y = 0;
 let op = "";
 
+let displayValue = document.getElementById('display')
+let calcButton = document.querySelector('.calcButton').addEventListener('click', buttonOp())
 
+
+function buttonOp(b) {
+    if (b)
+}
 
 function operate(op, x, y) {
     return op(x, y);
